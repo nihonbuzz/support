10ff770ed73ab178465bc20c844e43969d1fa02a
nihonbuzz <nihonbuzz.org@gmail.com>
2025-07-12 23:14:38 +0700

    v 1.0.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://support.nihonbuzz.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -61,22 +61,7 @@ const config = {
           editUrl:
             'https://github.com/nihonbuzz/support/tree/main/',
         },
-        blog: false, 
-        // {
-        //   showReadingTime: true,
-        //   feedOptions: {
-        //     type: ['rss', 'atom'],
-        //     xslt: true,
-        //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/nihonbuzz/support/tree/main/',
-        //   // Useful options to enforce blogging best practices
-        //   onInlineTags: 'warn',
-        //   onInlineAuthors: 'warn',
-        //   onUntruncatedBlogPosts: 'warn',
-        // },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -102,7 +87,6 @@ const config = {
             position: 'left',
             label: 'Pusat Bantuan',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/nihonbuzz/support',
             label: 'GitHub',
